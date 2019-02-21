6f53c80ccad6de0bb39086a4d25f245f30497811
Kit Jonathan Te <kjonathante@gmail.com>
2019-02-21 13:09:20 -0800

    Created test infrastructure

--- server/service.js
+++ server/service.js
@@ -1,21 +1,23 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const service = express();  
-const ServiceRegistry = require('./serviceRegistry');
+const express = require("express");
+const service = express();
+const ServiceRegistry = require("./serviceRegistry");
 const serviceRegistry = new ServiceRegistry();
 
-service.set('serviceRegistry', serviceRegistry);
+module.exports = config => {
+  service.set("serviceRegistry", serviceRegistry);
 
-service.put('/service/:intent/:port', (req, res, next) => {
+  service.put("/service/:intent/:port", (req, res, next) => {
     const serviceIntent = req.params.intent;
     const servicePort = req.params.port;
 
-    const serviceIp = req.connection.remoteAddress.includes('::')
-    ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+    const serviceIp = req.connection.remoteAddress.includes("::")
+      ? `[${req.connection.remoteAddress}]`
+      : req.connection.remoteAddress;
 
     serviceRegistry.add(serviceIntent, serviceIp, servicePort);
-    res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`});
-});
-
-module.exports = service;
+    res.json({ result: `${serviceIntent} at ${serviceIp}:${servicePort}` });
+  });
+  return service;
+};
